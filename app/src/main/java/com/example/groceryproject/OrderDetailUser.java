ec4d8bc1363d601494c1429060619d31a987b68c
Sudais <sudaisali420@gmail.com>
2022-04-16 11:18:51 -0700

    Grocery Store App

---
+++ app/src/main/java/com/example/groceryproject/OrderDetailUser.java
@@ -0,0 +1,14 @@
+package com.example.groceryproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OrderDetailUser extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_order_detail_user);
+    }
+}
